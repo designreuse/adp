2f5106ecef8a797861f4a6c4de88ac5130a17e38
Muthuraj <muthu_raj27@yahoo.co.in>
2015-09-11 15:33:35 +0530

    fixed the select issue

--- interval-b2b/src/main/webapp/js/app.js
+++ interval-b2b/src/main/webapp/js/app.js
@@ -142,3 +142,17 @@ app.controller('ServicesCtrl', function ($scope, $compile) {
     console.log('inside Services controller');
 });
 
+app.directive('convertToNumber', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function(val) {
+                return parseInt(val, 10);
+            });
+            ngModel.$formatters.push(function(val) {
+                return '' + val;
+            });
+        }
+    };
+});
+
